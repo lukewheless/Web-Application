3e8648d427b31573c4e97b7b82ed6dc7d2e5b992
Wheless <Luke_Wheless@baylor.edu>
2020-12-02 14:45:18 -0600

    enter

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -55,6 +55,9 @@ def new_topic(request):
 @login_required 
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = TopicForm() #blank form
